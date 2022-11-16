cc60e9854a4ad1dfc1b06f97566f29aa6e97be1e
Febrila Sucia <febrilasucia@gmail.com>
2022-11-16 18:46:21 +0700

    add viewBlog

--- src/pages/ViewBlog.jsx
+++ src/pages/ViewBlog.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect} from 'react';
-import Footer from '../components/Footer'
+import React, { useState, useEffect } from "react";
+import Footer from "../components/Footer";
 import axios from "axios";
 
 function ViewBlog() {
-  const [isLoading, setisLoading] = useState(true)
-  const [blogs, setBlogs] = useState([])
+  const [isLoading, setisLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     axios("https://634a6eef33bb42dca403ecb0.mockapi.io/blogMSWellbeing").then(
@@ -19,35 +19,35 @@ function ViewBlog() {
   console.log(blogs);
 
   const clickBlog = (item) => {
-    console.log("Berhasil di klik")
-    //console.log(item)
-  }
-  
- return (
-   <div>
-     {isLoading ? (
-       <span>Sedang mengambil data</span>
-     ) : (
-       blogs.map((item, index) => (
-         <div key={index} onClick={() => clickBlog(item)}>
-           <div className="font-serif px-5 pt-5 text-lg sm:text-4xl font-bold text-textPrimary text-center">
-             {item.tittle}
-           </div>
-           <div>
-             <div className="font-serif px-5 font-semibold text-sm text-[#71717a] text-center mt-2">
-               {item.releaseDate}
-             </div>
-             <div className="font-serif px-5 italic text-center">
-               Author by : {item.author}
-             </div>
-             <div className="flex flex-wrap justify-center w-full">
-               <img src={item.img} alt="" />
-             </div>
-           </div>
-         </div>
-       ))
-     )}
-     {/* <Navbar />
+    console.log("Berhasil di klik");
+    console.log(item);
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <span>Sedang mengambil data</span>
+      ) : (
+        blogs.map((item, index) => (
+          <div key={index} onClick={() => clickBlog(item)}>
+            <div className="font-serif px-5 pt-5 text-lg sm:text-4xl font-bold text-textPrimary text-center">
+              {item.tittle}
+            </div>
+            <div>
+              <div className="font-serif px-5 font-semibold text-sm text-[#71717a] text-center mt-2">
+                {item.releaseDate}
+              </div>
+              <div className="font-serif px-5 italic text-center">
+                Author by : {item.author}
+              </div>
+              <div className="flex flex-wrap justify-center w-full">
+                <img src={item.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))
+      )}
+      {/* <Navbar />
         <div>
             <h1 className='italic'>Judul</h1>
             <p className=''>Author | Tanggal</p>
@@ -58,8 +58,8 @@ function ViewBlog() {
             <div className='desc'></div>
         <Footer />
         </div> */}
-   </div>
- );
+    </div>
+  );
 }
 
-export default ViewBlog
+export default ViewBlog;
